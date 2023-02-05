47520f3acd531e2718ea6c01ce79f6c76fe5387a
Prashant Dalavi <p.d.dalavi@gmail.com>
2023-02-05 19:29:49 +0530

    bubble sort added

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array)
+	n = array.length
+
+	loop do 
+		#[1,4,1,3,4,1,3,3]
+		#[1,1,4,3,4,1,3,3]
+		#[1,1,3,4,4,1,3,3]...
+		swapped = false 
+
+		(n-1).times do |i|
+			if array[i] > array[i+1]
+				array[i], array[i+1] = array[i+1], array[i]
+				swapped= true
+			end
+		end
+		break if not swapped
+	end 
+	array
+end
+
+a = [2,7,2,3,6,9,5,8,1,2,4,8,5,3,6]
+p bubble_sort(a)
